1e1ea02855ae34162cf84fdace4f9101767b00d8
Nestor Parra <fabri0176@gmail.com>
2021-06-29 19:53:08 -0500

    Array methods reduce, ejemplo suma puntaje de paises

--- JS/ArrayMethods.js
+++ JS/ArrayMethods.js
@@ -4,14 +4,14 @@ const ArrayMethods = {
         { pais: 'Colombia', puntaje: 4, },
         { pais: 'Brasil', puntaje: 7, },
         { pais: 'Ecuador', puntaje: 4, },
-        { pais: 'Peru', puntaje: 5, }
+        { pais: 'Peru', puntaje: 6, }
     ],
     findIndexMethod() {
         const index = this.dias.findIndex(dia => dia === 'Jueves');
-        
+
         let msg = `El indice del día Jueves ${index}`;
 
-        if(index === -1){
+        if (index === -1) {
             msg = "No existe el día Jueves";
         }
 
@@ -19,14 +19,20 @@ const ArrayMethods = {
     },
     findIndexMethod2() {
         const index = this.equipos.findIndex(equipo => equipo.pais === 'Ecuador');
-        
+
         let msg = `El indice del equipo Ecuador ${index}`;
 
-        if(index === -1){
+        if (index === -1) {
             msg = "No existe el equipo Ecuador";
         }
 
         console.log(msg);
+    },
+    reduceMethod() {
+        //Suma de puntaje de todos los equipos
+        let totalPuntos = this.equipos.reduce((puntaje, equipo) =>  puntaje + equipo.puntaje, 0 );
+
+        console.log(`Total puntaje equipos: ${totalPuntos}`);
     },
     init() {
         console.log('<---- START findIndexMethod ---->');
@@ -34,6 +40,9 @@ const ArrayMethods = {
 
         console.log('<---- START findIndexMethod2 ---->');
         this.findIndexMethod2();
+
+        console.log('<---- START reduceMethod ---->');
+        this.reduceMethod();
     }
 };
 
