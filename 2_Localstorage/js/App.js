c1f0227c719996dee9ebfd63bf77e456bb0b4078
Néstor Fabricio Parra <nparra@existaya.com>
2021-08-03 21:32:38 -0500

    Se agrega funcionalidad listar los tweets guardados, botones eliminar y funcionalidad + boton de editar, pendiente de implementación funcionalidad

--- 2_Localstorage/js/App.js
+++ 2_Localstorage/js/App.js
@@ -3,13 +3,91 @@ const app = {
     tablaMensajes: undefined,
     agregar(e) {
         e.preventDefault();
-        console.log(e);
-    },
-    eliminar() {
+        const title = document.querySelector('#title');
+        const msg = document.querySelector('#msg');
+        let tweet = {
+            title: title.value,
+            msg: msg.value,
+        };
 
+        title.value = '';
+        msg.value = '';
+
+        app.setTweets(tweet);
+        app.listar();
+    },
+    setTweets(tweet = {}) {
+        let tweets = this.getTweets(); //Obtener tweets de localstorage
+        const id = tweets.length + 1; //Asignar id
+        tweet = { id, ...tweet }; // agregar id al objeto
+        tweets = [...tweets, tweet]; //Agregar tweet al consolidado
+
+        localStorage.setItem('tweets', JSON.stringify(tweets)); //Asignar en localstorage
+    },
+    getTweets() {
+        let tweets = JSON.parse(localStorage.getItem('tweets'));
+
+        if (tweets === null) {
+            return [];
+        }
+
+        return tweets;
+    },
+    eliminarAction(element) {
+        const id = element.getAttribute('data-id'); //ID Tweet
+        this.eliminar(id);
+        this.listar();
+    },
+    eliminar(id) {
+        let tweets = this.getTweets();
+        tweets = tweets.filter(tweet => { 
+            if(tweet.id != id){
+                return true;
+            }
+            return false;
+        },id);
+        console.log(tweets);
+
+
+        localStorage.setItem('tweets', JSON.stringify(tweets)); //Asignar en localstorage
+    },
+    editarAction(element){
+        const id = element.getAttribute('data-id'); //ID Tweet
+        console.log(id);
+    },
+    editar(){
+
+    },
+    getTablaMensajes() {
+        return this.tablaMensajes.getElementsByTagName('tbody')[0];
     },
     listar() {
+        this.limpiarTabla();
+        console.log('Listar');
+        const tweets = this.getTweets();
+        console.log(tweets);
+        const tbody = this.getTablaMensajes();
 
+        let bodyHtml = '';
+        tweets.forEach((tweet, index) => {
+            bodyHtml += `<tr>
+                            <td>${tweet.id}</td>
+                            <td>${tweet.title}</td>
+                            <td>${tweet.msg}</td>
+                            <td>
+                                <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                                    <button type="button" class="btn btn-warning" data-id='${tweet.id}' onclick="app.editarAction(this)">Editar</button>
+                                    <button type="button" class="btn btn-danger" data-id='${tweet.id}' onclick="app.eliminarAction(this)">X</button>
+                                </div>
+                            </td>
+                        </tr>`;
+        });
+
+        tbody.innerHTML = bodyHtml;
+    },
+    limpiarTabla() {
+        const tbody = this.getTablaMensajes();
+        tbody.innerHTML = '';
     },
     registrarEventos() {
         this.formulario.addEventListener('submit', this.agregar);
@@ -22,6 +100,7 @@ const app = {
         console.log('INIT');
         this.cargarVariables();
         this.registrarEventos();
+        this.listar();
     }
 };
 
